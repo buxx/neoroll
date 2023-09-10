6dcf68037ba413d0e8c05eff40f39f09962a259d
Bastien Sevajol <bastien.sevajol@algoo.fr>
2023-09-10 23:23:08 +0200

    feat: add camera

--- neoroll_client/src/main.rs
+++ neoroll_client/src/main.rs
@@ -6,18 +6,37 @@ fn main() {
     App::new()
         .add_plugins((DefaultPlugins, TilesetPlugin::default()))
         .init_resource::<RegionTileset>()
-        .add_systems(Startup, load_tileset)
-        .add_systems(Update, show_tileset)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (show_tileset, move_camera))
         .run();
+}
+
+#[derive(Component)]
+struct CameraController;
+
+fn setup(
+    mut tileset: ResMut<RegionTileset>,
+    asset_server: Res<AssetServer>,
+    mut commands: Commands,
+) {
+    commands.spawn((Camera2dBundle::default(), CameraController));
+    tileset.handle = Some(asset_server.load("tilesets/regions.ron"));
+}
+
+fn move_camera(
+    mut camera: Query<&mut Transform, With<CameraController>>,
+    button: Res<Input<MouseButton>>,
+) {
+    let mut camera = camera.single_mut();
+    if button.just_pressed(MouseButton::Left) {
+        println!("move camera");
+        camera.translation = camera.translation.lerp(Vec3::new(1.0, 1.0, 1.0), 0.2);
+    }
 }
 
 #[derive(Resource, Default)]
 struct RegionTileset {
     handle: Option<Handle<Tileset>>,
-}
-
-fn load_tileset(mut tileset: ResMut<RegionTileset>, asset_server: Res<AssetServer>) {
-    tileset.handle = Some(asset_server.load("tilesets/regions.ron"));
 }
 
 fn show_tileset(
