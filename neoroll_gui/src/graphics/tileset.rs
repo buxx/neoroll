bc23b835b943900430fc602e36fbae40f2889dca
Bastien Sevajol <bastien.sevajol@algoo.fr>
2023-09-11 00:00:58 +0200

    refactor: simplify tileset load

--- neoroll_gui/src/graphics/tileset.rs
+++ neoroll_gui/src/graphics/tileset.rs
@@ -12,21 +12,11 @@ pub fn show_tileset(
     tileset: Res<RegionTileset>,
     mut has_ran: Local<bool>,
 ) {
-    if tileset.handle.is_none() || *has_ran || !tilesets.contains_name("Regions") {
+    if *has_ran {
         return;
     }
 
-    let handle = tileset.handle.as_ref().unwrap();
-    if tilesets.get(handle).is_some() {
-        println!("Got tileset by handle! ({:?})", tileset.handle);
-    }
-    if let Some(tileset) = tilesets.get_by_id(&0) {
-        println!("Got tileset by ID! ({})", tileset.id());
-    }
-    if let Some(tileset) = tilesets.get_by_name("Regions") {
-        println!("Got tileset by name! ({})", tileset.name());
-        println!("{:#?}", tileset);
-
+    if let (Some(_), Some(tileset)) = (&tileset.handle, tilesets.get_by_name("Regions")) {
         let texture = tileset.texture().clone();
         commands.spawn(Camera2dBundle::default());
         commands.spawn(SpriteBundle {
